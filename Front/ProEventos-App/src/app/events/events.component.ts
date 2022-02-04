80a672032d7f7e082ea3517589048386100ff69a
Edvin <eduardo.vinicius2009@hotmail.com>
2022-02-04 14:26:09 -0300

    Adição de componentes do front (navbar, lista de eventos, ...)

--- Front/ProEventos-App/src/app/events/events.component.ts
+++ Front/ProEventos-App/src/app/events/events.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsComponent implements OnInit {
 
-  public events: any;
+  public events: any = [];
+  public filteredEvents: any = [];
+
+  imageWidth: number = 80;
+  imageMargin: number = 5;
+  showImage: boolean = true;
+  private _listFilter: string = '';
+
+  public get listFilter() : string  {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredEvents = this.listFilter ? this.filterEvents(this.listFilter) : this.events;
+  }
+
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.events.filter(
+      (event: any) => event.subject.toLocaleLowerCase().indexOf(filterBy) !== -1
+        || event.location.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +39,17 @@ export class EventsComponent implements OnInit {
     this.getEvents();
   }
 
+  changeImage() {
+    this.showImage = !this.showImage;
+  }
+
   public getEvents(): void {
     this.http.get('https://localhost:5001/api/events').subscribe(
-      response => this.events = response,
+      response =>
+      {
+        this.events = response
+        this.filteredEvents = this.events;
+      },
       error => console.log(error)
     )
   }
